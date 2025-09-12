bcce8e6decdc058cb10c9cc1aa28a73403383d84
Abdullahjamal9 <127139769+Abdullahjamal9@users.noreply.github.com>
2025-09-12 15:54:33 +0500

    Update streamlit_testing_app.py

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -643,7 +643,7 @@ else:
             st.markdown(
                 f"""
                 <div style="padding:20px; border-radius:12px; background: linear-gradient(135deg, #3B82F6, #2563EB, #1E3A8A); color:white; text-align:center; margin-top:20px;">
-                    <h3 style="color:{color};"><b>Final Result : {status}</b></h3>
+                    <h3 style="color:{color};"><b>Final Result :</b> <b>{status}</b></h3>
                     <p style="font-size:18px;">
                         <b>Score :</b> {right}/{total_q}<br>
                         <b>Percentage :</b> {pct:.2f}%<br>
