ab87eeab5efec37f84563e1276125544486a71e3
Abdullahjamal9 <127139769+Abdullahjamal9@users.noreply.github.com>
2025-09-17 10:43:27 +0500

    Update streamlit_testing_app.py

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -514,7 +514,7 @@ if st.session_state.admin_logged_in:
                 """
                 <style>
                 div[data-testid="column"]:nth-child(4) button {
-                    margin-top: 4.75rem !important;
+                    margin-top: 5px !important;
                 }
                 </style>
                 """,
