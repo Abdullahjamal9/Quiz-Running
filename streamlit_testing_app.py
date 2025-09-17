0b0a30c65e1da4910dad942ff9ab510daf83f96b
Abdullahjamal9 <127139769+Abdullahjamal9@users.noreply.github.com>
2025-09-17 10:33:25 +0500

    Update streamlit_testing_app.py

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -67,7 +67,7 @@ def load_employees_and_standards():
                     </div>
                     """,
                     unsafe_allow_html=True
-                ) employees.empty or not any(col.lower() in ["id", "name"] for col in employees.columns):
+                employees.empty or not any(col.lower() in ["id", "name"] for col in employees.columns):
                 employees = pd.DataFrame(columns=["ID", "Name"])
             else:
                 id_col = next((col for col in employees.columns if "id" in col.lower()), "ID")
