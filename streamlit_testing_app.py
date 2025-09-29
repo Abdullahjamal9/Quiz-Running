c881ef026e8e1cceee142dbb8156f3114f45341e
Abdullahjamal9 <127139769+Abdullahjamal9@users.noreply.github.com>
2025-09-29 11:27:46 +0500

    Update streamlit_testing_app.py

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -374,7 +374,7 @@ def generate_certificate(emp_id, emp_name, test_date, status, template_type):
             align_date = fitz.TEXT_ALIGN_RIGHT
         
         # Increased font size for dates
-        date_font_size = 24  # Increased from 18
+        date_font_size = 21  # Increased from 18
         replacements[old_date] = (new_date, arial_font, date_font_size, align_date, (0,0,0), (1,1,1))
 
         # Certificate number - remove padding and increase font size with flexible search
