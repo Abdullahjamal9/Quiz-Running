c947d79a73c210b7b08d128e5b98c50936b7020f
Abdullahjamal9 <127139769+Abdullahjamal9@users.noreply.github.com>
2025-10-14 13:15:29 +0500

    Update streamlit_testing_app.py

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -463,15 +463,15 @@ def generate_certificate(
                 date_rect = fitz.Rect(
                     date_label.x0,
                     date_label.y0,
-                    date_label.x0 + date_label.width + 120,
+                    date_label.x0 + date_label.width + 150,
                     date_label.y1
                 )
                 # Adjust height if needed
-                fs = 16
-                # if date_rect.height < fs * 1.1:
-                #     cy = (date_rect.y0 + date_rect.y1) / 2
-                #     date_rect.y0 = cy - fs
-                #     date_rect.y1 = cy + fs
+                fs = 14
+                if date_rect.height < fs * 1.1:
+                    cy = (date_rect.y0 + date_rect.y1) / 2
+                    date_rect.y0 = cy - fs
+                    date_rect.y1 = cy + fs
                 
                 # Apply redaction with new text
                 page.add_redact_annot(
