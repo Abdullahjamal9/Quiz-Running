589d141dd7acea4a39e78d980bbe00c59e772ec4
Abdullahjamal9 <127139769+Abdullahjamal9@users.noreply.github.com>
2025-10-14 12:55:16 +0500

    Update streamlit_testing_app.py

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -467,11 +467,11 @@ def generate_certificate(
                     date_label.y1
                 )
                 # Adjust height if needed
-                fs = 14
+                fs = 16
                 if date_rect.height < fs * 1.1:
                     cy = (date_rect.y0 + date_rect.y1) / 2
-                    date_rect.y0 = cy - fs + 1000
-                    date_rect.y1 = cy + fs + 1000
+                    date_rect.y0 = cy - fs * 0.5
+                    date_rect.y1 = cy + fs * 0.5
                 
                 # Apply redaction with new text
                 page.add_redact_annot(
